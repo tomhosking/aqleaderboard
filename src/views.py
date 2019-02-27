90caae70fce06b6d7bf1ef7e11dd859a6755a3e0
Tom Hosking <tomhosking@gmail.com>
2019-02-27 15:24:08 +0000

    Fix data path

--- src/views.py
+++ src/views.py
@@ -1,10 +1,14 @@
 from flask import render_template
 
-import json
+import json, os
+
+CURRENT_FILE = os.path.abspath(__file__)
+CURRENT_DIR = os.path.dirname(CURRENT_FILE)
+PROJECT_DIR = os.path.dirname(CURRENT_DIR)
 
 def squad_board():
 
-    with open('../data/scores.json') as fp:
+    with open(PROJECT_DIR+'/data/scores.json') as fp:
         scores = json.load(fp)
 
     ranked = [{ **x, 'rank': ix+1} for ix, x in enumerate(sorted(scores, key=lambda x: x['bleu_score'], reverse=True))]
