da210dd0a40b1ad98b809c7610067d998a25c94b
Tom Hosking <tomhosking@gmail.com>
2019-02-27 13:21:22 +0000

    Change prod paths

--- src/server.py
+++ src/server.py
@@ -3,12 +3,12 @@ import sys
 
 CURRENT_FILE = os.path.abspath(__file__)
 CURRENT_DIR = os.path.dirname(CURRENT_FILE)
-PROJECT_DIR = os.path.dirname(CURRENT_DIR)
+PROJECT_DIR = os.path.dirname(os.path.dirname(CURRENT_DIR))
 
 # Where our files are located.
-sys.path.append(PROJECT_DIR )
+sys.path.append(CURRENT_DIR )
 
 # Location of the virtualenv.
-sys.path.append(PROJECT_DIR + '../../aqvenv/lib/python3.7/site-packages/')
+sys.path.append(PROJECT_DIR + 'aqvenv/lib/python3.7/site-packages/')
 
 from app import app as application
